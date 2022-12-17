dc09bd4a713ae34080f4faeef788ef6dfe5e81f4
Vladimir Irkhin <ivb1989@yandex.ru>
2022-12-17 18:16:12 +0300

    Add two script for analysis csv. Have two version old and new.

--- analysis_csv.py
+++ analysis_csv.py
@@ -1,55 +1,47 @@
+"""Анализ csv файла и для контракта, кд, договора вытаскивать только цифры с разбиением по столбцам"""
+
 import re
 import pandas as pd
 import time
 
+
 start_time = time.time()
 
-# паттерн первый для договора, используется в общем случае и универсален
-contract_pattern = "((дог|кд|к\/д|догN|КД|КД_|к\.д|к\д)?\s+(№|N)?\s?(\d){2,}\s+(от\s?(\w{2}(\.|\/|\,)){2}\w{2,4}))"
 # паттерн фио
 fio_pattern = "([А-ЯЁ][А-Яа-яёЁ]+\s{1,2}?([А-ЯЁ][А-Яа-яёЁ]+\s?|[А-ЯЁ]+\.|[А-ЯЁ]+\,|[А-ЯЁ]+|[А-ЯЁ]+\/){2})"
-# второй паттерн работает для отдельных случаев, чтобы добавить больше договор в итоговый файл
-contract_pattern2 = "(дог|кд|к\/д|догN|КД|КД_|к\.д|к\д|N|№|Дог\.|кд\.|Дог\.№|дог\.№|КД\s+№)([\s|\d|\.|\-|от|ОЦ|НД|/|_|N|Z|PHA|УПД]{3,})"
+
+# паттерн для финального вытаскивания только цифр кд,договора или контракта без дат
+contract_pattern = "(\d{3,})"
+
+# года для исключения в финальном паттерне
+years = set([year for year in range(2000, 2023, 1)])
 
 
 def analysis_csv_file(in_file):
     """Анализ csv файла и запись данных в списки: фио, контракт и общие данные"""
     # объявляем списки, куда будем записывать все фио, контракты и все считанные данные
-    all_fio = list()
-    all_data = list()
-    all_contract = list()
+    all_fio, all_data, all_contract = list(), list(), list()
 
     # считываем файл
     df = pd.read_csv(in_file, sep=';', encoding='cp1251', keep_default_na=False)
 
     for line in df['comments']:
-        print(line)
-
-        fio = re.findall(fio_pattern, str(line))
-        print(fio)
-
-        fio_text = '; '.join(map(str, [f[0].strip() for f in fio]))
-        print(fio_text)
-
-        contract = re.findall(contract_pattern, str(line))
-        print(contract)
-
-        # если для контракта не срабатывает первый паттерн для анализа, то использую второй
         try:
-            if len(contract) == 0:
-                contract = re.findall(contract_pattern2, str(line))
-                print(contract)
-                # если не срабатывает второй, то записываем пустую строку
-                if len(contract) != 0:
-                    contract_text = ''.join(map(str, contract[0])).strip()
-                else:
-                    contract_text = ''
-            else:
-                contract_text = '; '.join(map(str, [c[0].strip() for c in contract])).strip()
+            print(line)
+
+            fio = re.findall(fio_pattern, str(line))
+            print(fio)
+
+            fio_text = '; '.join(map(str, [f[0].strip() for f in fio]))
+            print(fio_text)
+
+            contract = re.findall(contract_pattern, str(line))
+            print(contract)
+
+            contract_text = ';'.join(map(str, [c.strip() for c in contract if int(c) not in years]))
+            print(contract_text)
         except Exception as e:
             print('Перехват ошибок и выяснение, где проблема', e)
-        finally:
-            print(contract_text)
 
         all_fio.append(fio_text)
         all_data.append(line)
@@ -68,7 +60,7 @@ def create_csv_after_analysis(in_file, out_file):
 
 def main():
     input_file = 'regular0612.csv'
-    output_file = 'out_full6.csv'
+    output_file = 'out_full_final.csv'
     create_csv_after_analysis(input_file, output_file)
 
 
