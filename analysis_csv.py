11db00b82374d72f89fe028af17c2db698175ac1
Vladimir Irkhin <ivb1989@yandex.ru>
2023-03-11 15:56:00 +0300

    Modified main script with pre-commit. Add gitignore.

--- analysis_csv.py
+++ analysis_csv.py
@@ -1,47 +1,55 @@
-"""Анализ csv файла и для контракта, кд, договора вытаскивать только цифры с разбиением по столбцам"""
+"""Анализ csv файла и для контракта, кд, договора вытаскивать
+только цифры с разбиением по столбцам"""
 
 import re
-import pandas as pd
 import time
 
+import pandas as pd
 
 start_time = time.time()
 
 # паттерн фио
-fio_pattern = "([А-ЯЁ][А-Яа-яёЁ]+\s{1,2}?([А-ЯЁ][А-Яа-яёЁ]+\s?|[А-ЯЁ]+\.|[А-ЯЁ]+\,|[А-ЯЁ]+|[А-ЯЁ]+\/){2})"
+fio_pattern = r"([А-ЯЁ][А-Яа-яёЁ]+\s{1,2}?([А-ЯЁ][А-Яа-яёЁ]+\s?|[А-ЯЁ]+\.|[А-ЯЁ]+\,|[А-ЯЁ]+|[А-ЯЁ]+\/){2})"  # noqa: E501
 
-# паттерн для финального вытаскивания только цифр кд,договора или контракта без дат
-contract_pattern = "(\d{3,})"
+# паттерн для финального вытаскивания только цифр кд,договора
+# или контракта без дат
+contract_pattern = r"(\d{3,})"
 
 # года для исключения в финальном паттерне
-years = set([year for year in range(2000, 2023, 1)])
+years = {year for year in range(2000, 2023, 1)}
 
 
 def analysis_csv_file(in_file):
-    """Анализ csv файла и запись данных в списки: фио, контракт и общие данные"""
-    # объявляем списки, куда будем записывать все фио, контракты и все считанные данные
+    """Анализ csv файла и запись данных в списки: фио, контракт
+    и общие данные"""
+    # объявляем списки, куда будем записывать все фио, контракты
+    # и все считанные данные
     all_fio, all_data, all_contract = list(), list(), list()
 
     # считываем файл
-    df = pd.read_csv(in_file, sep=';', encoding='cp1251', keep_default_na=False)
+    df = pd.read_csv(
+        in_file, sep=";", encoding="cp1251", keep_default_na=False
+    )  # noqa: E501
 
-    for line in df['comments']:
+    for line in df["comments"]:
         try:
             print(line)
 
             fio = re.findall(fio_pattern, str(line))
             print(fio)
 
-            fio_text = '; '.join(map(str, [f[0].strip() for f in fio]))
+            fio_text = "; ".join(map(str, [f[0].strip() for f in fio]))
             print(fio_text)
 
             contract = re.findall(contract_pattern, str(line))
             print(contract)
 
-            contract_text = ';'.join(map(str, [c.strip() for c in contract if int(c) not in years]))
+            contract_text = ";".join(
+                map(str, [c.strip() for c in contract if int(c) not in years])
+            )
             print(contract_text)
         except Exception as e:
-            print('Перехват ошибок и выяснение, где проблема', e)
+            print("Перехват ошибок и выяснение, где проблема", e)
 
         all_fio.append(fio_text)
         all_data.append(line)
@@ -54,13 +62,13 @@ def create_csv_after_analysis(in_file, out_file):
     """Создание файла на основании проанализированных данных"""
     data, fio, contract = analysis_csv_file(in_file)
 
-    df2 = pd.DataFrame({'comments': data, 'ФИО': fio, 'Договор(-а)': contract})
-    df2.to_csv(out_file, sep=';', encoding='cp1251')
+    df2 = pd.DataFrame({"comments": data, "ФИО": fio, "Договор(-а)": contract})
+    df2.to_csv(out_file, sep=";", encoding="cp1251")
 
 
 def main():
-    input_file = 'regular0612.csv'
-    output_file = 'out_full_final.csv'
+    input_file = "regular0612.csv"
+    output_file = "out_full_final.csv"
     create_csv_after_analysis(input_file, output_file)
 
 
--- analysis_csv_old.py
+++ analysis_csv_old.py
@@ -1,55 +1,64 @@
-"""Анализ csv файла и для контракта, кд, договора вытаскивать максимум информации, включая дату и тд."""
+"""Анализ csv файла и для контракта, кд, договора
+вытаскивать максимум информации, включая дату и тд."""
 
 import re
-import pandas as pd
 import time
+
+import pandas as pd
 
 start_time = time.time()
 
 # паттерн первый для договора, используется в общем случае и универсален
-contract_pattern_main = "((дог|кд|к\/д|догN|КД|КД_|к\.д|к\д)?\s+(№|N)?\s?(\d){2,}\s+(от\s?(\w{2}(\.|\/|\,)){2}\w{2,4}))"
+contract_pattern_main = r"((дог|кд|к\/д|догN|КД|КД_|к\.д|к\д)?\s+(№|N)?\s?(\d){2,}\s+(от\s?(\w{2}(\.|\/|\,)){2}\w{2,4}))"  # noqa: E501
 # паттерн фио
-fio_pattern = "([А-ЯЁ][А-Яа-яёЁ]+\s{1,2}?([А-ЯЁ][А-Яа-яёЁ]+\s?|[А-ЯЁ]+\.|[А-ЯЁ]+\,|[А-ЯЁ]+|[А-ЯЁ]+\/){2})"
-# второй паттерн работает для отдельных случаев, чтобы добавить больше договор в итоговый файл
-contract_pattern_other = "(дог|кд|к\/д|догN|КД|КД_|к\.д|к\д|N|№|Дог\.|кд\.|Дог\.№|дог\.№|КД\s+№)([\s|\d|\.|\-|от|ОЦ|НД|/|_|N|Z|PHA|УПД]{3,})"
+fio_pattern = r"([А-ЯЁ][А-Яа-яёЁ]+\s{1,2}?([А-ЯЁ][А-Яа-яёЁ]+\s?|[А-ЯЁ]+\.|[А-ЯЁ]+\,|[А-ЯЁ]+|[А-ЯЁ]+\/){2})"  # noqa: E501
+# второй паттерн работает для отдельных случаев, чтобы добавить больше договор в итоговый файл # noqa: E501
+contract_pattern_other = r"(дог|кд|к\/д|догN|КД|КД_|к\.д|к\д|N|№|Дог\.|кд\.|Дог\.№|дог\.№|КД\s+№)([\s|\d|\.|\-|от|ОЦ|НД|/|_|N|Z|PHA|УПД]{3,})"  # noqa: E501
 
 
 def analysis_csv_file(in_file):
-    """Анализ csv файла и запись данных в списки: фио, контракт и общие данные"""
-    # объявляем списки, куда будем записывать все фио, контракты и все считанные данные
+    """Анализ csv файла и запись данных в списки:
+    фио, контракт и общие данные"""
+    # объявляем списки, куда будем записывать все фио,
+    # контракты и все считанные данные
     all_fio = list()
     all_data = list()
     all_contract = list()
 
     # считываем файл
-    df = pd.read_csv(in_file, sep=';', encoding='cp1251', keep_default_na=False)
+    df = pd.read_csv(
+        in_file, sep=";", encoding="cp1251", keep_default_na=False
+    )  # noqa: E501
 
-    for line in df['comments']:
+    for line in df["comments"]:
         print(line)
 
         fio = re.findall(fio_pattern, str(line))
         print(fio)
 
-        fio_text = '; '.join(map(str, [f[0].strip() for f in fio]))
+        fio_text = "; ".join(map(str, [f[0].strip() for f in fio]))
         print(fio_text)
 
         contract = re.findall(contract_pattern_main, str(line))
         print(contract)
 
-        # если для контракта не срабатывает первый паттерн для анализа, то использую второй
+        # если для контракта не срабатывает первый паттерн для анализа,
+        # то использую второй
         try:
             if len(contract) == 0:
                 contract = re.findall(contract_pattern_other, str(line))
                 print(contract)
                 # если не срабатывает второй, то записываем пустую строку
                 if len(contract) != 0:
-                    contract_text = ''.join(map(str, contract[0])).strip()
+                    contract_text = "".join(map(str, contract[0])).strip()
                 else:
-                    contract_text = ''
+                    contract_text = ""
             else:
-                contract_text = '; '.join(map(str, [c[0].strip() for c in contract])).strip()
+                contract_text = "; ".join(
+                    map(str, [c[0].strip() for c in contract])
+                ).strip()
         except Exception as e:
-            print('Перехват ошибок и выяснение, где проблема', e)
+            print("Перехват ошибок и выяснение, где проблема", e)
         finally:
             print(contract_text)
 
@@ -64,13 +73,13 @@ def create_csv_after_analysis(in_file, out_file):
     """Создание файла на основании проанализированных данных"""
     data, fio, contract = analysis_csv_file(in_file)
 
-    df2 = pd.DataFrame({'comments': data, 'ФИО': fio, 'Договор(-а)': contract})
-    df2.to_csv(out_file, sep=';', encoding='cp1251')
+    df2 = pd.DataFrame({"comments": data, "ФИО": fio, "Договор(-а)": contract})
+    df2.to_csv(out_file, sep=";", encoding="cp1251")
 
 
 def main():
-    input_file = 'regular0612.csv'
-    output_file = 'out_full6.csv'
+    input_file = "regular0612.csv"
+    output_file = "out_full6.csv"
     create_csv_after_analysis(input_file, output_file)
 
 
